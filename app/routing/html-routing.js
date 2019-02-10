f97da65ca2a30c88ef23a304a46221ebabfeed2b
William Tovar <william.tovar002@mymdc.net>
2019-02-09 21:48:04 -0500

    end3

---
+++ app/routing/html-routing.js
@@ -0,0 +1,23 @@
+
+
+var path = require('path');
+
+
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function(app){
+
+
+	app.get('/survey', function(req, res){
+		res.sendFile(path.join(__dirname + '/../public/survey.html'));
+	});
+
+	
+	app.use(function(req, res){
+		res.sendFile(path.join(__dirname + '/../public/home.html'));
+	});
+
+}
